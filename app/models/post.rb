15d8ab0ad37511ad3aa3b4aa28fe34fce9fbf51f
Sean Oughton <seanoughton@gmail.com>
2018-04-25 22:19:46 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,16 +6,9 @@ class Post < ActiveRecord::Base
     message: "%{value} is not a valid category" }
   validate :is_clickbait?
 
-   CLICKBAIT_PATTERNS = [
-     /Won't Believe/i,
-     /Secret/i,
-     /Top [0-9]*/i,
-     /Guess/i
-   ]
+   
 
    def is_clickbait?
-     #binding.pry
-     #if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
      if title
        if !title.include?("Won't" && "Believe") && !title.include?("Secret" || "Guess") && !title.include?("Won't" && "Believe") && !title.include?("/Top [0-9]*/")
          errors.add(:title, "must be clickbait")
