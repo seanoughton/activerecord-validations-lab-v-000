74e5f1d9005a903f3c803b759109487a9c3bc1e7
Sean Oughton <seanoughton@gmail.com>
2018-04-25 21:53:18 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid category" }
+  validate :is_clickbait?
+
+  def is_clickbait?
+    if title != "Won't Believe"
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
